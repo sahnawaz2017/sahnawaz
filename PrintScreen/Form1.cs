bc736b939146df4eff0c0ef610b827c01cbb06a2
Sahnawaz Khan <Sahnawaz@hotmail.com>
2017-06-14 01:29:30 -0500

    1st commit

---
+++ PrintScreen/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PrintScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+            bool success = false;
+            int i=1;
+            while (!success)
+            {
+                if (File.Exists(@"C:\Temp\" + i + ".jpg"))
+                    i++;
+                else
+                    success = true;
+            }
+            printscreen.Save(@"C:\Temp\" + i + ".jpg", ImageFormat.Jpeg);
+        }
+    }
+}
